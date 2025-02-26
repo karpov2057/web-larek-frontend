21160ca33ac6105b44f0c56c4be2e0f0101e072e
karpov2057 <karpov2057@yandex.ru>
2025-02-26 17:33:56 +0500

    отправка документации на проверку

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+interface IProduct {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number;
+}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,9 @@
-export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
-export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
+export const API_URL = `${process.env.API_ORIGIN}/api/weblarek/cohort30`;
+export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek/cohort30`;
 
 export const settings = {
-
+  headers: {
+    autorization: `${process.env.API_TOKEN}`,
+    "Content-Type": "application/json",
+  },
 };
